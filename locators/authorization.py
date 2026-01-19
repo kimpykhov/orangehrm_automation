84adcb2428588531d0063529cf0940eb85e8a0bf
Kim Pykhov <pykhovk@gmail.com>
2026-01-19 22:15:05 +0200

    locators + configs

--- locators/authorization.py
+++ locators/authorization.py
@@ -1,4 +1,4 @@
-class LoginPageLocatos:
+class LoginPageLocators:
     USERNAME_INPUT = "input[name='username']"
     PASSWORD_INPUT = "input[name='password']"
 
@@ -6,3 +6,15 @@ class LoginPageLocatos:
     LOGIN_FORGOT = "div[class='orangehrm-login-forgot']"
 
     LOGO = "img[alt='company-branding']"
+
+    SIDE_LOGO = "div[class='orangehrm-login-logo']"
+
+    USERNAME_REQUIRED_ERROR = (
+        "div.oxd-input-group:has(label:text('Username')) "
+        "span.oxd-input-field-error-message"
+    )
+
+    PASSWORD_REQUIRED_ERROR = (
+        "div.oxd-input-group:has(label:text('Password')) "
+        "span.oxd-input-field-error-message"
+    )
