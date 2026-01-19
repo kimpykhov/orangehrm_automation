91a4060ae208ce3e901152aac1770bf2fd86cfb8
Kim Pykhov <pykhovk@gmail.com>
2026-01-19 21:28:33 +0200

    some authorization page locators

---
+++ locators/authorization.py
@@ -0,0 +1,8 @@
+class LoginPageLocatos:
+    USERNAME_INPUT = "input[name='username']"
+    PASSWORD_INPUT = "input[name='password']"
+
+    LOGIN_BUTTON = "button[type='submit']"
+    LOGIN_FORGOT = "div[class='orangehrm-login-forgot']"
+
+    LOGO = "img[alt='company-branding']"
